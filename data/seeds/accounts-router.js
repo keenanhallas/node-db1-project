cd5177e1664c03eeda1c4826eb9dbcb97951cd36
Keenan <keenanhallas@gmail.com>
2020-09-07 11:48:19 -0500

    all endpoints working

--- data/seeds/accounts-router.js
+++ data/seeds/accounts-router.js
@@ -17,4 +17,73 @@ router.get("/", (req, res) => {
         });
 });
 
+router.get("/:id", (req, res) => {
+    const accountId = req.params.id;
+
+    db.select('*')
+        .from('accounts')
+        .where({ id: accountId })
+        .then(accounts => {
+            res.status(200).json({ data: accounts });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send({ error: err.message });
+        });
+});
+
+router.post("/", (req, res) => {
+    account = req.body;
+
+    db('accounts')
+        .insert(account, 'id')
+        .returning('id')
+        .then(ids => {
+            res.status(201).json({ inserted: ids });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send({ error: err.message });
+        });
+});
+
+router.put("/:id", (req, res) => {
+    const changes = req.body;
+    const accountId = req.params.id;
+
+    db('accounts')
+        .where({ id: accountId })
+        .update(changes)
+        .then(count => {
+            if (count) {
+                res.status(200).json({ message: "Updated successfully!" });
+            } else {
+                res.status(404).json({ message: "Account not found!" });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send({ error: err.message });
+        });
+});
+
+router.delete("/:id", (req, res) => {
+    const accountId = req.params.id;
+
+    db('accounts')
+        .where({ id: accountId })
+        .del()
+        .then(count => {
+            if (count) {
+                res.status(200).json({ message: 'Deleted successfully' });
+            } else {
+                res.status(404).json({ message: 'Not found' });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err.message });
+        });
+});
+
 module.exports = router;
